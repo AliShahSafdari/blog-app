2d26a788127abea4ac23a05031280afe88251d0c
Alishah Safdari <alishahsafdari905@gmail.com>
2023-08-17 22:27:08 +0430

    Complete comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,13 @@
-class Post < ApplicationRecord
+class Comment < ApplicationRecord
+    belongs_to :post
+    belongs_to :author, class_name: 'User'
+
+
+    after_save :update_post_comment_counter
+
+    private
+
+    def update_post_comment_counter
+        post.increment!(:comment_counter)
+    end
 end
