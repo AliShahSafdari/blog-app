741542901b550e075d54672204b93ffe53af86e1
Alishah Safdari <alishahsafdari905@gmail.com>
2023-08-17 22:27:55 +0430

    Complete like  model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,12 @@
-class Post < ApplicationRecord
+class Like < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    belongs_to :post
+
+    after_save :update_post_like_counter
+
+    private
+
+    def update_post_like_counter
+        post.increment!(:like_counter)
+    end
 end
