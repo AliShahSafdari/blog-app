085fa4b70f59e463e66df7da84948198a215ec8c
Alishah Safdari <alishahsafdari905@gmail.com>
2023-08-18 10:52:32 +0430

    Add validation to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User'
+  validates :text, presence: true, length: { in: 3..100 }
 
   after_save :update_post_comment_counter
 
