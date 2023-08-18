b9a0c007687198a5b167f65cfe6821b0245769e8
Alishah Safdari <alishahsafdari905@gmail.com>
2023-08-18 10:49:50 +0430

    Add validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
+  validates :name, presence: true, length: { in: 3..25 }
+  validates :photo, presence: true, length: { in: 5..50 }
+  validates :bio, presence: true, length: { in: 10..255 }
+  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
   end
