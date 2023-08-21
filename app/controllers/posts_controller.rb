46c684fd4485e34ba996615cbf8d4a2691df6081
Alishah Safdari <alishahsafdari905@gmail.com>
2023-08-21 18:48:35 +0430

    Fix linter error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,4 @@
 class PostsController < ActionController::Base
-
-    def index; end
-
-    def show; end
-
+  def index; end
+  def show; end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ActionController::Base
+  def index; end
 
-    def index; end
-
-    def show; end
-
+  def show; end
 end
