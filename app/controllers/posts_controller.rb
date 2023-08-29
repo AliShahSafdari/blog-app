903c56829351b3f70684b6bccb09129ee058fc30
Alishah Safdari <alishahsafdari905@gmail.com>
2023-08-29 20:57:18 +0430

    Update post controller and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,17 @@
 class PostsController < ApplicationController
   def index
+    page = params[:page] || 1
+    per_page = 10
+
     @posts = Post.includes(:author)
       .includes(:comments)
       .where(author: params[:user_id])
       .order(created_at: :asc)
+      .offset((page.to_i - 1) * per_page)
+      .limit(per_page)
 
-    @author = @posts.first.author
+    @total_pages = (current_user.posts.count.to_f / per_page).ceil
+    @author = @posts.first.author unless @posts.first.nil?
   end
 
   def show
