37d5d7915e2228a809ab3a891d5e377943eb8d92
Alishah Safdari <alishahsafdari905@gmail.com>
2023-08-31 08:22:19 +0430

    Update application controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  
+         :recoverable, :rememberable, :validatable  
   # Associations
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
@@ -11,8 +10,8 @@ class User < ApplicationRecord
   
   # Attributes and Validations
   validates :name, presence: true, length: { in: 3..25 }
-  validates :photo, presence: true
-  validates :bio, presence: true, length: { in: 10..255 }
+  # validates :photo, presence: true
+  # validates :bio, presence: true, length: { in: 10..255 }
   validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   # Methods
