da1431a75cbf1690fbe9c0c13d963e98539b6583
Alishah Safdari <alishahsafdari905@gmail.com>
2023-09-02 09:22:53 +0430

    Update Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_devise_parameters, if: :devise_controller?
+
   def configure_devise_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user|
-      user.permit(:name, :email, :password, :password_confirmation)
+      user.permit(:name, :email, :password, :password_confirmation, :role)
     end
   end
+
 end
